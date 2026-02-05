39ae1ea201315ff71b8d63b60db1e5def6318e51
CZelaya-GD <carlosfernando2233@gmail.com>
2026-02-05 18:35:33 +0000

    Changes to be committed: 	modified:   csv_cleaner.py 	modified:   input.csv 	new file:   output.csv  "Day 1: Adding documentation and failsafes"

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -1,10 +1,33 @@
 import pandas as pd
 
+# First ETL pipeline
+
 def clean_csv(input_file, output_file):
-
+    # Extract: Read raw data
     df = pd.read_csv(input_file)
-    df = df.dropna(), df.drop_duplicates()
+    print(f"Raw: {len(df)} rows")
+
+    #Transform: Drop bad data
+    df = df.dropna().drop_duplicates()
+    print(f"Clean: {len(df)} rows")
+
+    #Load: Save clean data
     df.to_csv(output_file, index=False)
+    print(f"Saved to {output_file}")
+    return df
 
-    print(f"Cleaned {len(df)} rows to {output_file}")
 
+# Creating dummy test data
+if __name__ == "__main__":
+
+    # REMOVE for production
+    print("Creating test data...")
+    test_data = pd.DataFrame({
+        "name": ["Alice", "Bob", "Charlie","", "Bob"],
+        "age": [22,None, 25, 35, 31]
+    })
+
+    test_data.to_csv("input.csv", index=False)
+
+    # Run ETL
+    clean_csv("input.csv", "output.csv")
